4a9959190061fe4eb3498cbebdbb3e58983815b3
sparda0117 <luisborrell0117@gmail.com>
2026-02-20 16:14:07 -0600

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
 import { Routes, Route } from 'react-router-dom';
 import Reportes from './Reportes';
+import Peticiones from './Peticiones'; // <-- Agregamos esta importación
 
 function App() {
   return (
     <Routes>
-      {/* Esta es tu página principal actual */}
       <Route path="/" element={<Reportes />} />
-      
-      {/* Este es el espacio reservado para el nuevo formulario */}
-      <Route path="/peticiones" element={<div className="text-white p-10 text-xl font-bold">Página de peticiones en construcción... 🚧</div>} />
+      <Route path="/peticiones" element={<Peticiones />} /> {/* <-- Actualizamos esta ruta */}
     </Routes>
   );
 }
